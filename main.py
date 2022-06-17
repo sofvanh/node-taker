b50d8e9506bba0863f4e8d18af5844e2cbaf83ef
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-06-17 16:33:14 +0200

    Can add nodes and edges from command line

--- main.py
+++ main.py
@@ -16,11 +16,42 @@ def write_data_to_file(data):
     f.close()
 
 
+def add_node(data, name):
+    data["nodes"].append(name)
+
+
+def add_edge(data, edge):
+    # TODO ensure edge is valid (both nodes exist)
+    data["edges"].append(edge)
+
+
 if __name__ == '__main__':
-    print("We're testing out graphs :)\n")
+    print("Welcome to your knowledge graph <3")
+    print("Commands: 'n' for new node, 'e' for new edge, 's' for save & exit")
 
     data = get_data_from_file()
     drawer_pyvis.draw_graph(data["nodes"], data["edges"])
-    write_data_to_file(data)
 
-    print("\nDone! See you soon!")
+    i = input("Enter command:")
+
+    while i != 'q':
+        if i == 'n':
+            name = input("Name of new node:")
+            add_node(data, name)
+            drawer_pyvis.draw_graph(data["nodes"], data["edges"])
+        elif i == 'e':
+            u = input("Name of first node:")
+            v = input("Name of second node:")
+            title = input("Title for the edge:")
+            edge = {"u": u, "v": v, "title": title}
+            add_edge(data, edge)
+            drawer_pyvis.draw_graph(data["nodes"], data["edges"])
+        elif i == 's':
+            break
+        else:
+            print("Sorry, I didn't understand :( Try 'n' for new node, 'e' for new edge, or 's' for save & exit.")
+
+        i = input("Enter command:")
+
+    write_data_to_file(data)
+    print("Done! See you soon!")
