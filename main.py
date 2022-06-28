530beadbf773b22611c388da8c1e3e0f2689e9df
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-06-28 15:10:19 +0300

    Command for showing visuals + code style fix

--- main.py
+++ main.py
@@ -16,31 +16,30 @@ def add_edge(data, edge):
 
 if __name__ == '__main__':
     print("Welcome to your knowledge graph <3")
-    print("Commands: 'n' for new node, 'e' for new edge, 's' for save & exit")
+    print("Commands: 'n' for new node, 'e' for new edge, 'l' for load visuals, 's' for save & exit")
 
-    data = file_helper.get_data_from_file("data.json")
+    data = file_helper.get_data_from_file("data/data.json")
     drawer_pyvis.draw_graph(data["nodes"], data["edges"])
 
-    i = input("Enter command:")
+    i = ''
 
     while i != 'q':
+        i = input("Enter command:")
         if i == 'n':
             name = input("Name of new node:")
             add_node(data, name)
-            drawer_pyvis.draw_graph(data["nodes"], data["edges"])
         elif i == 'e':
             u = input("Name of first node:")
             v = input("Name of second node:")
             title = input("Title for the edge:")
             edge = {"u": u, "v": v, "title": title}
             add_edge(data, edge)
+        elif i == 'l':
             drawer_pyvis.draw_graph(data["nodes"], data["edges"])
         elif i == 's':
             break
         else:
             print("Sorry, I didn't understand :( Try 'n' for new node, 'e' for new edge, or 's' for save & exit.")
-
-        i = input("Enter command:")
 
     file_helper.write_data_to_file(data)
     print("Done! See you soon!")
